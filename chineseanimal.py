2a1058bd3805038918ff745ad2845627a9e732ae
crisvasi <crisvasi@yahoo.com>
2021-06-12 14:38:01 -0600

    cambio

--- chineseanimal.py
+++ chineseanimal.py
@@ -4,7 +4,7 @@
 # Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 # session persistence, api calls, and more.
 # This sample is built using the handler classes approach in skill builder.
-import logging
+
 import ask_sdk_core.utils as ask_utils
 
 from ask_sdk_core.skill_builder import SkillBuilder
@@ -13,9 +13,6 @@ from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 from ask_sdk_core.handler_input import HandlerInput
 
 from ask_sdk_model import Response
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
 
 class LaunchRequestHandler(AbstractRequestHandler):
